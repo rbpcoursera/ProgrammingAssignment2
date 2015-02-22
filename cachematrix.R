23d982d16053af389f45115e08fddb02c2decce8
rbpotter <robertbrucepotter@gmail.com>
2015-02-22 14:05:46 -0800

    Complete implementation for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Provides the facilities for constructing a matrix that caches its inverse
+## as well as invalidating the inverse once the matrix changes
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Constructs a wrapper around a matrix which caches it's inverse
+makeCacheMatrix <- function(val = matrix()) 
+{
+    cachedInverse <- NULL
+  
+    set <- function(new_val) 
+    {
+        val <<- new_val
+        cachedInverse <<- NULL
+    }
+  
+    get <- function() val
+  
+    setInverse <- function(inverse) cachedInverse <<- inverse
+  
+    # Personally, I think that cacheSolve should be defined here locally
+    # but not be put into the function list. Instead, when getInverse is
+    # invoked, it should invoke cacheSolve (which would access local data
+    # directly rather than using the functions)
+    getInverse <- function() cachedInverse
+  
+    list(
+        set = set, 
+        get = get,
+        setInverse = setInverse,
+        getInverse = getInverse
+    )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return the inverse of the wrapped matrix defined above
+## Returns either the cached result of the last invocation, or it will compute
+## the inverse and cache it for future access (until the matrix changes)
+cacheSolve <- function(mat, ...) 
+{
+    inverse <- mat$getInverse()
+    
+    if(!is.null(inverse)) 
+    {
+        print("Cached")
+        return(inverse)
+    }
+    
+    # The function template takes varargs, but I'm not going to forward them
+    # here since that would potentially change the meaning of the function
+    # (caching a simple Matrix multiply whose result is transient)
+    inverse <- solve(mat$get())
+    mat$setInverse(inverse)
+    inverse
 }
